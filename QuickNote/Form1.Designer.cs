0b0813bfe48ccc28925e8239b8a2f03c0eb2690d
levendowskil3713 <60446780+levendowskil3713@users.noreply.github.com>
2021-02-08 17:54:23 -0600

    Test push - Created a basic window

--- QuickNote/Form1.Designer.cs
+++ QuickNote/Form1.Designer.cs
@@ -29,13 +29,32 @@ namespace QuickNote
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
+      this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+      this.SuspendLayout();
+      // 
+      // richTextBox1
+      // 
+      this.richTextBox1.Location = new System.Drawing.Point(12, 47);
+      this.richTextBox1.Name = "richTextBox1";
+      this.richTextBox1.Size = new System.Drawing.Size(776, 391);
+      this.richTextBox1.TabIndex = 0;
+      this.richTextBox1.Text = "";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.richTextBox1);
+      this.Name = "Form1";
       this.Text = "Form1";
+      this.ResumeLayout(false);
+
     }
 
     #endregion
+
+    private System.Windows.Forms.RichTextBox richTextBox1;
   }
 }
 
