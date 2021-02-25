f58b3450f4917c61b75bdc71adf1662e81ed62bd
levendowskil3713 <60446780+levendowskil3713@users.noreply.github.com>
2021-02-25 12:22:42 -0600

    Updated documentation
    Updated our existing functions in Form1.cs to meet our new documentation standards.

--- QuickNote/Form1.cs
+++ QuickNote/Form1.cs
@@ -20,9 +20,11 @@ namespace QuickNote
       InitializeComponent();
     }
 
-    /**
-     * Initializes utilized UI component values
-     */
+    /// <summary>
+    /// Initializes utilized UI component values.
+    /// </summary>
+    /// <param name="sender">A reference to the object that raised the event.</param>
+    /// <param name="e">Contains the function's event data.</param>
     private void QuickNote_Load(object sender, EventArgs e)
     {
       //Sets the font size of the main text box
@@ -32,17 +34,21 @@ namespace QuickNote
       FontSizeComboBox.Text = string.Format("{0:N0}", MainTextBox.Font.Size);
     }
 
-    /**
-     * Bolds all selected text and updates button color
-     */
+    /// <summary>
+    /// Bolds all selected text and updates button color.
+    /// </summary>
+    /// <param name="sender">A reference to the object that raised the event.</param>
+    /// <param name="e">Contains the function's event data.</param>
     private void BoldButton_Click(object sender, EventArgs e)
     {
+      //un-bolds selected text if the selected text is currently bolded
       if (MainTextBox.SelectionFont.Bold == true)
       {
         MainTextBox.SelectionFont = new Font(MainTextBox.Font.FontFamily, fontSize, MainTextBox.SelectionFont.Style & ~FontStyle.Bold);
         BoldButton.BackColor = Color.White;
       }
 
+      //bolds selected text if the selected text is not currently bolded
       else
       {
         MainTextBox.SelectionFont = new Font(MainTextBox.Font.FontFamily, fontSize, MainTextBox.SelectionFont.Style | FontStyle.Bold);
@@ -50,17 +56,21 @@ namespace QuickNote
       }
     }
 
-    /**
-     * Underlines all selected text and updates button color
-     */
+    /// <summary>
+    /// Underlines all selected text and updates button color.
+    /// </summary>
+    /// <param name="sender">A reference to the object that raised the event.</param>
+    /// <param name="e">Contains the function's event data.</param>
     private void UnderlineButton_Click(object sender, EventArgs e)
     {
+      //un-underlines selected text if the selected text is currently underlined
       if (MainTextBox.SelectionFont.Underline == true)
       {
         MainTextBox.SelectionFont = new Font(MainTextBox.Font.FontFamily, fontSize, MainTextBox.SelectionFont.Style & ~FontStyle.Underline);
         UnderlineButton.BackColor = Color.White;
       }
 
+      //underlines selected text if the selected text is not currently underlined
       else
       {
         MainTextBox.SelectionFont = new Font(MainTextBox.Font.FontFamily, fontSize, MainTextBox.SelectionFont.Style | FontStyle.Underline);
@@ -68,17 +78,21 @@ namespace QuickNote
       }
     }
 
-    /**
-     * Italicizes all selected text and updates button color
-     */
+    /// <summary>
+    /// Italicizes all selected text and updates button color.
+    /// </summary>
+    /// <param name="sender">A reference to the object that raised the event.</param>
+    /// <param name="e">Contains the function's event data.</param>
     private void ItalicsButton_Click(object sender, EventArgs e)
     {
+      //un-italicizes selected text if the selected text is currently italicizes
       if (MainTextBox.SelectionFont.Italic == true)
       {
         MainTextBox.SelectionFont = new Font(MainTextBox.Font.FontFamily, fontSize, MainTextBox.SelectionFont.Style & ~FontStyle.Italic);
         ItalicsButton.BackColor = Color.White;
       }
 
+      //italicizes selected text if the selected text is not currently italicizes
       else
       {
         MainTextBox.SelectionFont = new Font(MainTextBox.Font.FontFamily, fontSize, MainTextBox.SelectionFont.Style | FontStyle.Italic);
@@ -86,11 +100,14 @@ namespace QuickNote
       }
     }
 
-    /**
-     * Sets selected text to the specified size
-     */
+    /// <summary>
+    /// Sets selected text to the specified size in the text size combo box.
+    /// </summary>
+    /// <param name="sender">A reference to the object that raised the event.</param>
+    /// <param name="e">Contains the function's event data.</param>
     private void FontSizeComboBox_SelectedIndexChanged(object sender, EventArgs e)
     {
+      //only changes text size when the input can be parsed as a float
       try
       {
         fontSize = float.Parse(FontSizeComboBox.Text);
@@ -103,10 +120,11 @@ namespace QuickNote
       }
     }
 
-
-    /**
-     * Updates the color and/or text of pertinent UI elements when new text is selected on the main text box
-     */
+    /// <summary>
+    /// Updates the color and/or text of pertinent UI elements when new text is selected on the main text box.
+    /// </summary>
+    /// <param name="sender">A reference to the object that raised the event.</param>
+    /// <param name="e">Contains the function's event data.</param>
     private void MainTextBox_SelectionChanged(object sender, EventArgs e)
     {
       //updates text of font size combo box
