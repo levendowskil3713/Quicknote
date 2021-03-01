71c5697a3a1fb734aa35d1fff8d7478e2909e6d9
Robert Lillyblad <robert.lillyblad@gmail.com>
2021-03-01 09:44:49 -0600

    Added basic drawing function

--- QuickNote/Form1.cs
+++ QuickNote/Form1.cs
@@ -14,10 +14,17 @@ namespace QuickNote
   {
     //font size of the main text box
     float fontSize = 11;
+    Graphics g;
+    int x = -1;
+    int y = -1;
+    bool moving = false;
+    Pen pen;
 
     public MainForm()
     {
       InitializeComponent();
+      g = panel1.CreateGraphics();
+      pen = new Pen(Color.Black, 5);
     }
 
     /**
@@ -142,5 +149,61 @@ namespace QuickNote
         UnderlineButton.BackColor = Color.White;
       }
     }
-  }
+
+        private void Panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            moving = false;
+            x = -1;
+            y = -1;
+
+        }
+
+        private void Panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            moving = true;
+            x = e.X;
+            y = e.Y;
+        }
+
+        private void Panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (moving && x != -1 && y != -1) 
+            {
+                g.DrawLine(pen, new Point(x, y),e.Location);
+                x = e.X;
+                y = e.Y;
+            }
+        }
+
+        private void CursorButton_Click(object sender, EventArgs e)
+        {
+            if (CursorButton.Checked == false)
+                panel1.BringToFront();
+            else
+                MainTextBox.BringToFront();
+        }
+
+        private void RedToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            pen.Color = b.BackColor;
+        }
+
+        private void LineColorSplitButton_ButtonClick(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            pen.Color = b.BackColor;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            TextBox t = (TextBox)sender;
+            pen.Color = t.BackColor;
+        }
+    }
 }
